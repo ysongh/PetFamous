7286778e3c74c5a0edceaaf958fe0a97fbbdf368
You Song Hou <yousonghou@yahoo.com>
2021-04-28 22:21:25 -0400

    install skynet-js and add functions to get and set user data on SkyDB

--- README.md
+++ README.md
@@ -5,6 +5,11 @@
 npm install
 ```
 
+## Create a file called 'config.js' on the src folder and add the following code
+```
+export const seedphase = "your words";
+```
+
 ### Compiles and hot-reloads for development
 ```
 npm run serve
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,42 @@
 <template>
   <div class="home">
     <h1>Pet Famous</h1>
+    <button @click="setJSONFromSkyDB()">Set Data</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import { SkynetClient, genKeyPairFromSeed } from "skynet-js";
+import { seedphase } from '../config';
+
+const portal = 'https://siasky.net/';
+const client = new SkynetClient(portal);
+const { privateKey, publicKey } = genKeyPairFromSeed(seedphase);
+const dataKey = "localhost";
 
 export default {
   name: 'Home',
-  components: {
-    
+  methods: {
+    async setJSONFromSkyDB(){
+      try {
+        const json = {
+          message: "It works"
+        };
+        const { data, skylink } = await client.db.setJSON(privateKey, dataKey, json);
+        console.log(data, skylink);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+  async created() {
+    try {
+      const { data, skylink } = await client.db.getJSON(publicKey, dataKey);
+      console.log(data, skylink);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 </script>
