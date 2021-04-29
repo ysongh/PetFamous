ce66242dba7a9b71a5cdd5ec351eb5d3f04eaebb
You Song Hou <yousonghou@yahoo.com>
2021-04-28 20:42:13 -0400

    set up the project with vue

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Pet Famous</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
